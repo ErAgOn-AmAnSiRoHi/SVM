8ed9a198cb4cbf1d0775b6b2ec84002c9c9c4f2e
Aman Sirohi <95467701+ErAgOn-AmAnSiRoHi@users.noreply.github.com>
2023-01-04 19:49:53 +0530

    Create HardMarginL1SVM.m

---
+++ Linear SVM/HardMarginL1SVM.m
@@ -0,0 +1,23 @@
+d_val = [-1;-1;-1;1;1;1];
+d = diag(d_val)
+A = [1 2;2 1;2 2;3 3;3 4;4 3]
+e = ones(6,1)
+
+cvx_begin
+    variables w(2) g
+    minimize((w'*w)/2)
+    subject to
+    d*(A*w - g*e) >= 1
+cvx_end
+
+w
+g
+
+x=[0;0];
+s=sign((w'*x)-g)
+
+if s==1
+    disp("class is one ")
+else
+    disp("class is zero ")
+end
